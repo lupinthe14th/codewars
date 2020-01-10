c7186690d06a9e291011a62e1c743c18d2d5177e
Hideo Suzuki <hideosuzuki@ordinarius-fectum.net>
2020-01-10 15:54:12 +0900

    feat(6 kyu:WeIrD StRiNg CaSe): I could self solve this problem (#5)
     I could self solve this problem

---
+++ kata/weird-string-case/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import "strings"
+
+func toWeirdCase(str string) string {
+
+	arr := make([]string, 0, len(str))
+
+	for _, s := range strings.Split(str, " ") {
+		arr = append(arr, weirdCase(s))
+	}
+	return strings.Join(arr, " ")
+}
+
+func weirdCase(str string) string {
+	var ans string
+	for i, r := range str {
+		if i&1 == 0 {
+			ans += strings.ToUpper(string(r))
+		} else {
+			ans += strings.ToLower(string(r))
+		}
+	}
+	return ans
+}
+
+func main() {}
---
+++ kata/weird-string-case/solution_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Case struct {
+	input string
+	want  string
+}
+
+var cases = []Case{
+	{input: "abc def", want: "AbC DeF"},
+	{input: "ABC", want: "AbC"},
+	{input: "This is a test Looks like you passed", want: "ThIs Is A TeSt LoOkS LiKe YoU PaSsEd"},
+}
+
+func TestToWeirdCase(t *testing.T) {
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := toWeirdCase(tt.input)
+			if got != tt.want {
+				t.Errorf("%s, want: %s", got, tt.want)
+			}
+		})
+	}
+}
