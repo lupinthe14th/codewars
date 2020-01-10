6d1176f3065a87a5e8d8eb6e42478e875d0cd562
Hideo Suzuki <hideosuzuki@ordinarius-fectum.net>
2020-01-10 14:39:21 +0900

    feat(Bit Counting): I could self solve this problem (#4)
     I could self solve this problem

---
+++ kata/bit-counting/solution_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Case struct {
+	input uint
+	want  int
+}
+
+var cases = []Case{
+	{input: 0, want: 0},
+	{input: 4, want: 1},
+	{input: 7, want: 3},
+	{input: 9, want: 2},
+	{input: 10, want: 2},
+}
+
+func TestCountBits(t *testing.T) {
+	for i, tt := range cases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := CountBits(tt.input)
+			if got != tt.want {
+				t.Errorf("%d, want: %d", got, tt.want)
+			}
+		})
+
+	}
+}
