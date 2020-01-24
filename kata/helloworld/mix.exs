5bdd84445ec28734716012dfd384706a362d91e3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-24 23:59:57 +0900

    fix(CI): add dep

--- kata/helloworld/mix.exs
+++ kata/helloworld/mix.exs
@@ -8,6 +8,9 @@ defmodule Helloworld.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      lockfile: Path.expand("mix.lock", __DIR__),
+      deps_path: Path.expand("deps", __DIR__),
+      build_path: Path.expand("_build", __DIR__),
       test_coverage: [tool: ExCoveralls]
     ]
   end
@@ -24,7 +27,8 @@ defmodule Helloworld.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:excoveralls, "~> 0.12.2", only: [:dev, :test]}
+      {:excoveralls, "~> 0.12.2", only: [:dev, :test]},
+      {:credo, "~> 0.6", only: [:dev, :test]}
     ]
   end
 end
