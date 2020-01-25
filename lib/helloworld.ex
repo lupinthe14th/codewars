b387e974e2f7b8cf751b83cdd21683d27c10ad88
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-25 16:06:09 +0900

    improve(8 kyu/Function 1 - hello world): Using String.downcase
    because the coverage is from 0% to 100%

--- lib/helloworld.ex
+++ lib/helloworld.ex
@@ -1,18 +1,5 @@
 defmodule HelloWorld do
-  @moduledoc """
-  Documentation for HelloWorld.
-  """
-
-  @doc """
-  hello world!
-
-  ## Examples
-
-      iex> HelloWorld.greet()
-      "hello world!"
-
-  """
   def greet do
-    "hello world!"
+    String.downcase("hello world!")
   end
 end
