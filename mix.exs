84458935d662132537316317c051825b7f9a4a0e
Hideo Suzuki <hideosuzuki@ordinarius-fectum.net>
2020-01-28 15:16:00 +0900

    Ci (#11)

--- mix.exs
+++ mix.exs
@@ -11,7 +11,9 @@ defmodule Codewars.MixProject do
       lockfile: Path.expand("mix.lock", __DIR__),
       deps_path: Path.expand("deps", __DIR__),
       build_path: Path.expand("_build", __DIR__),
-      test_coverage: [tool: ExCoveralls]
+      preferred_cli_env: preferred_cli_env(),
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: dialyzer()
     ]
   end
 
@@ -22,13 +24,30 @@ defmodule Codewars.MixProject do
     ]
   end
 
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test,
+      "coveralls.json": :test
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "plts/dialyzer.plt"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:excoveralls, "~> 0.12.2", only: [:dev, :test]},
-      {:credo, "~> 0.6", only: [:dev, :test]}
+      {:credo, "~> 0.6", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
