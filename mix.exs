9f1e7b2f3a58241aea8aa13e3c6ea53687e051fb
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-01-28 12:41:36 +0900

    fix(dialyzer): add ci configuration

--- mix.exs
+++ mix.exs
@@ -11,7 +11,9 @@ defmodule Codewars.MixProject do
       lockfile: Path.expand("mix.lock", __DIR__),
       deps_path: Path.expand("deps", __DIR__),
       build_path: Path.expand("_build", __DIR__),
-      test_coverage: [tool: ExCoveralls]
+	  preferred_cli_env: preferred_cli_env(),
+	    test_coverage: [tool: ExCoveralls],
+	    dialyzer: dialyzer()
     ]
   end
 
@@ -22,6 +24,21 @@ defmodule Codewars.MixProject do
     ]
   end
 
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test,
+      "coveralls.json": :test
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "plts/dialyzer.plt"}
+    ]
+  end
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
