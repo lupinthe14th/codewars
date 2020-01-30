e3f7d74dd6b4951b5675d296a196be319ba3d9b2
Hideo Suzuki <hideosuzuki@ordinarius-fectum.net>
2020-01-30 12:37:47 +0900

    feat(8 kyu/Generate range of integers): I couldn't self solve this problem (#17)
    ## Approach: recursion
    ### SeeAlso:
    - https://elixir-lang.org/getting-started/recursion.html
    - https://www.codewars.com/kata/reviews/58058d36d0070d471b00002e/groups/5870ea72468562786c00116a
    
    ## Approach: Enum
    ### SeeAlso:
    - https://hexdocs.pm/elixir/Enum.html#take_every/2

---
+++ test/numgenerator_test.exs
@@ -0,0 +1,34 @@
+defmodule TestNumGenerater do
+  use ExUnit.Case
+  import NumGenerator, only: [generate_range: 3]
+
+  test "basic cases" do
+    assert generate_range(2, 10, 2) == [2, 4, 6, 8, 10]
+    assert generate_range(1, 10, 3) == [1, 4, 7, 10]
+    assert generate_range(1, 10, 1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert generate_range(1, 10, 4) == [1, 5, 9]
+    assert generate_range(1, 10, 5) == [1, 6]
+  end
+
+  defp solution(min, max, step) do
+    min
+    |> Stream.unfold(&{&1, &1 + step})
+    |> Enum.take_while(&(&1 <= max))
+  end
+
+  test "random cases" do
+    1..40
+    |> Enum.each(fn _ ->
+      min = :rand.uniform(20)
+      max = :rand.uniform(70) + 30
+      step = :rand.uniform(10)
+      answer = solution(min, max, step)
+
+      assert generate_range(min, max, step) == answer,
+             "Got #{inspect(generate_range(min, max, step), limit: 100)} instead of #{
+               inspect(answer, limit: 100)
+             } " <>
+               "for min: #{min}, max: #{max}, and step: #{step}"
+    end)
+  end
+end
