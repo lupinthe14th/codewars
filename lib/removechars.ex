9dfb101d03bab7dfcf63134e40420c36dc207348
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-02-02 22:27:38 +0900

    fix(8 kyu/Remove First and Last Character): Line is too long (max is 120, was 239).

--- lib/removechars.ex
+++ lib/removechars.ex
@@ -1,5 +1,8 @@
 defmodule RemoveChars do
-  # It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+  # It's pretty straightforward. Your goal is to create a function that removes
+  # the first and last characters of a string. You're given one parameter, the
+  # original string. You don't have to worry with strings with less than two
+  # characters.
   def remove_chars(s) do
     s
     |> String.slice(1..-2)
