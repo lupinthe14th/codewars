c6d4ea933f60f4df2a664f8e6172b5e718f471a3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-02-04 16:22:23 +0900

    improve(6 kyu/Two Sum): refactoe code
    approach: One-pass Hash Table
    reference: https://www.codewars.com/kata/reviews/5c06ba6387fb5ddba8002486/groups/5c0e387110e90fdaed001e0c

--- kata/two-Sum/solution.go
+++ kata/two-Sum/solution.go
@@ -2,18 +2,14 @@ package twoSum
 
 func TwoSum(numbers []int, target int) [2]int {
 	ans := [2]int{}
-	l := len(numbers)
-	m := make(map[int]int, l)
-
-	for i, v := range numbers {
-		m[v] = i
-	}
+	m := make(map[int]int, len(numbers))
 
 	for i, v := range numbers {
 		if j, ok := m[target-v]; ok {
-			ans = [2]int{i, j}
+			ans = [2]int{j, i}
 			break
 		}
+		m[v] = i
 	}
 	return ans
 }
