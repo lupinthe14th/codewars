87e55a172ee53ee2382e7de50f52a504e8e682b3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-02-12 23:31:32 +0900

    feat(Heronizer): approach: arithmetic & recursion

---
+++ lib/heronizer.ex
@@ -0,0 +1,9 @@
+defmodule Heronizer do
+  def no_boring_zeros(0), do: 0
+
+  def no_boring_zeros(n) when rem(n, 10) == 0 do
+    no_boring_zeros(div(n, 10))
+  end
+
+  def no_boring_zeros(n), do: n
+end
