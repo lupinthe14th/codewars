c2f870c529d1a84b4ab5705c757a684ebbc11908
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-03-27 20:01:38 +0900

    feat(8 kyu/Triple Trouble): approach: seen: https://www.codewars.com/kata/reviews/59739e6658b1848aa6000aa1/groups/597c5c9634a4b03e2e000062

---
+++ test/weirdstring_test.exs
@@ -0,0 +1,16 @@
+defmodule TestWeirdString do
+  use ExUnit.Case
+  import WeirdString, only: [triple_trouble: 3]
+
+  test "basic cases" do
+    assert triple_trouble("this", "test", "lock") == "ttlheoiscstk"
+    assert triple_trouble("aa", "bb", "cc") == "abcabc"
+    assert triple_trouble("Bm", "aa", "tn") == "Batman"
+    assert triple_trouble("LLh", "euo", "xtr") == "LexLuthor"
+    assert triple_trouble("aaa", "bbb", "ccc") == "abcabcabc"
+    assert triple_trouble("aaaaaa", "bbbbbb", "cccccc") == "abcabcabcabcabcabc"
+    assert triple_trouble("burn", "reds", "roll") == "brrueordlnsl"
+    assert triple_trouble("Sea", "urn", "pms") == "Supermans"
+    assert triple_trouble("LLh", "euo", "xtr") == "LexLuthor"
+  end
+end
