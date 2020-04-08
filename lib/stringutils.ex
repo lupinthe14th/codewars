9bd38b006dc077b1d67b1c4726e0a81d14d4fa76
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-04-08 17:54:28 +0900

    8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe: using recursion with String.to_charlist

--- lib/stringutils.ex
+++ lib/stringutils.ex
@@ -2,4 +2,15 @@ defmodule StringUtils do
   def upper_case?(str) do
     str == String.upcase(str, :default)
   end
+
+  def alter_case(str) do
+    str
+    |> String.to_charlist()
+    |> Enum.map(&_alter(&1))
+    |> List.to_string()
+  end
+
+  defp _alter(c) when c in ?a..?z, do: c - 32
+  defp _alter(c) when c in ?A..?Z, do: c + 32
+  defp _alter(c), do: c
 end
