649b0239e446e85b4b6c28316b79b97f55f1ed66
Hideo Suzuki <hideosuzuki@ordinarius-fectum.net>
2020-04-08 17:56:16 +0900

    8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe: using recursion with String.to_charlist (#47)
    …ng.to_charlist

--- test/stringutils_test.exs
+++ test/stringutils_test.exs
@@ -1,6 +1,6 @@
 defmodule TestStringUtils do
   use ExUnit.Case
-  import StringUtils, only: [upper_case?: 1]
+  import StringUtils, only: [upper_case?: 1, alter_case: 1]
 
   test "basic cases from description" do
     assert upper_case?("") == true
@@ -10,5 +10,18 @@ defmodule TestStringUtils do
     assert upper_case?("HELLO I AM DONALD") == true
     assert upper_case?("ACSKLDFJSgSKLDFJSKLDFJ") == false
     assert upper_case?("ACSKLDFJSGSKLDFJSKLDFJ") == true
+  end
+
+  test "basic cases" do
+    assert alter_case("hello world") == "HELLO WORLD"
+    assert alter_case("HELLO WORLD") == "hello world"
+    assert alter_case("hello WORLD") == "HELLO world"
+    assert alter_case("HeLLo WoRLD") == "hEllO wOrld"
+    # Non-alphabetical characters are unaffected
+    assert alter_case("12345") == "12345"
+    assert alter_case("1a2b3c4d5e") == "1A2B3C4D5E"
+    assert alter_case("StringUtils.toAlternatingCase") == "sTRINGuTILS.TOaLTERNATINGcASE"
+    # When method is called twice, should return original string
+    assert alter_case(alter_case("Hello World")) == "Hello World"
   end
 end
