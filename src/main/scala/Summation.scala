9ebe288916b74127fd0194c9f675024e278914ab
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-02 00:08:39 +0900

    solution: using while

--- src/main/scala/Summation.scala
+++ src/main/scala/Summation.scala
@@ -1,3 +1,11 @@
 object Summation {
-  def summation(n: Int): Int = ???
+  def summation(n: Int): Int = {
+    var i = 1
+    var cum = 0
+    while(i<= n) {
+      cum += i
+      i += 1
+    }
+    cum
+  }
 }
