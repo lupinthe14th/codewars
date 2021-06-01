c832d19e0b3e72b26e43078add4858cba5db0929
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-02 00:24:17 +0900

    improive: using scalafmt

--- src/test/scala/SummationSpec.scala
+++ src/test/scala/SummationSpec.scala
@@ -4,9 +4,9 @@ import matchers.should._
 
 class SummationSpec extends AnyFlatSpec with Matchers {
   "summation(1)" should "return 1" in {
-    Summation.summation(1) should be (1)
+    Summation.summation(1) should be(1)
   }
   "summation(8)" should "return 36" in {
-    Summation.summation(8) should be (36)
+    Summation.summation(8) should be(36)
   }
 }
