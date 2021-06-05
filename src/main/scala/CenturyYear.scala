1cfc48ab89d085385f3a176d0f74b7283ee0d364
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-05 09:08:42 +0900

    approach: arithmetic

--- src/main/scala/CenturyYear.scala
+++ src/main/scala/CenturyYear.scala
@@ -1,3 +1,9 @@
 object CenturyYear {
-  def centuryFromYear(year: Int): Int = ???
+  def centuryFromYear(year: Int): Int = {
+    val mod = year % 100
+    if (1 <= mod && mod < 100) {
+      return year / 100 + 1
+    }
+    return year / 100
+  }
 }
