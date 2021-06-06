8e96fb1d753e488507b5e72f0119083b2aa83f08
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-07 08:58:19 +0900

    refactor code

--- src/main/scala/Grasshopper.scala
+++ src/main/scala/Grasshopper.scala
@@ -1,8 +1,3 @@
 object Grasshopper {
-  def checkForFactor(base: Int, factor: Int): Boolean = {
-    if (base % factor == 0) {
-      return true
-    }
-    return false
-  }
+  def checkForFactor(base: Int, factor: Int): Boolean = base % factor == 0
 }
