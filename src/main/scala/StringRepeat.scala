67e49e73ee70fc18a7b3a2730edbf3eb5d073ccc
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-08 09:02:52 +0900

    approach: straightforward

--- src/main/scala/StringRepeat.scala
+++ src/main/scala/StringRepeat.scala
@@ -1,9 +1,3 @@
 object StringRepeat {
-  def repeatStr(times: Int, str: String): String = {
-    val result = new StringBuilder
-    for (i <- 1 to times) {
-      result.append(str)
-    }
-    result.toString
-  }
+  def repeatStr(times: Int, str: String): String = str * times
 }
