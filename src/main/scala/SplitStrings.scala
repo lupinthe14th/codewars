b533d23de5910c3b359161935b3d2d4bb6ae54cd
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-09 18:59:03 +0900

    approach: straightforward: using mod

--- src/main/scala/SplitStrings.scala
+++ src/main/scala/SplitStrings.scala
@@ -1,3 +1,9 @@
 object SplitStrings {
-  def solution(s: String): List[String] = ???
+  def solution(s: String): List[String] = {
+    if ((s.length % 2) != 0) {
+      val t = s + "_"
+      return t.sliding(2, 2).toList
+    }
+    s.sliding(2, 2).toList
+  }
 }
