b04855d9b3351d06115d988f9e3102baabbacb8c
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-06-10 14:28:19 +0900

    approach: using bitwise

--- src/main/scala/BitCounting.scala
+++ src/main/scala/BitCounting.scala
@@ -1,3 +1,14 @@
 object BitCounting {
-  def countBits(n: Int): Int = ???
+  // iビット目が1か調べる
+  // 1. 右ビットシフトしてiビット目を一番右に移動する（最下位ビットシフトする）
+  // 2. 最下位ビットを取り出すため、&1する（両方が1なら1）
+  def countBits(n: Int): Int = {
+    var c = 0
+    for (i <- 0 to n.toBinaryString.size) {
+      if (((n >> i) & 1) == 1) {
+        c += 1
+      }
+    }
+    c
+  }
 }
