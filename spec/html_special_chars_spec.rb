e6c794d9adde1a7f6c745ea3139623b4eaa111dc
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-10-08 14:10:40 +0900

    ci: fix rubocop offenses

--- spec/html_special_chars_spec.rb
+++ spec/html_special_chars_spec.rb
@@ -1,11 +1,19 @@
 require_relative '../lib/html_special_chars'
-require 'codewars_test'
 
 describe 'Sample tests' do
-  it 'Examples' do
+  it 'html h2' do
     expect(html_special_chars('<h2>Hello World</h2>')).to eq('&lt;h2&gt;Hello World&lt;/h2&gt;')
+  end
+
+  it 'text &' do
     expect(html_special_chars('Hello, how would you & I fare?')).to eq('Hello, how would you &amp; I fare?')
+  end
+
+  it 'text \"' do
     expect(html_special_chars('How was "The Matrix"?  Did you like it?')).to eq('How was &quot;The Matrix&quot;?  Did you like it?')
+  end
+
+  it 'html script' do
     expect(html_special_chars("<script>alert('Website Hacked!');</script>")).to eq("&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;")
   end
 end
