19b94797d90a7e2bf87c53f373e3f64d777fb73d
Hideo Suzuki <2602639+lupinthe14th@users.noreply.github.com>
2022-03-17 17:33:24 +0900

    8 kyu Opposite number (#91)

---
+++ tests/opposite.rs
@@ -0,0 +1,13 @@
+fn opposite(number: i32) -> i32 {
+    -number
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_expected() {
+        assert_eq!(opposite(1), -1);
+        assert_eq!(opposite(-1), 1);
+    }
+}
