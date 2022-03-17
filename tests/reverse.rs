bdc57f88c098eeb14b657fdc3d33d2bf27d991ca
lupinthe14th <2602639+lupinthe14th@users.noreply.github.com>
2022-03-17 18:30:38 +0900

    improve: for graphemes

--- tests/reverse.rs
+++ tests/reverse.rs
@@ -1,5 +1,7 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    input.graphemes(true).rev().collect()
 }
 
 fn process_reverse_case(input: &str, expected: &str) {
@@ -11,44 +13,36 @@ fn test_an_empty_string() {
     process_reverse_case("", "");
 }
 #[test]
-#[ignore]
 /// a word
 fn test_a_word() {
     process_reverse_case("robot", "tobor");
 }
 #[test]
-#[ignore]
 /// a capitalized word
 fn test_a_capitalized_word() {
     process_reverse_case("Ramen", "nemaR");
 }
 #[test]
-#[ignore]
 /// a sentence with punctuation
 fn test_a_sentence_with_punctuation() {
     process_reverse_case("I'm hungry!", "!yrgnuh m'I");
 }
 #[test]
-#[ignore]
 /// a palindrome
 fn test_a_palindrome() {
     process_reverse_case("racecar", "racecar");
 }
 #[test]
-#[ignore]
 /// an even-sized word
 fn test_an_even_sized_word() {
     process_reverse_case("drawer", "reward");
 }
 #[test]
-#[ignore]
 /// wide characters
 fn test_wide_characters() {
     process_reverse_case("子猫", "猫子");
 }
 #[test]
-#[ignore]
-#[cfg(feature = "grapheme")]
 /// grapheme clusters
 fn test_grapheme_clusters() {
     process_reverse_case("uüu", "uüu");
